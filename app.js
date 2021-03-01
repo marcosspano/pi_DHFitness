45926d13409faaa35c68f6a069e14b89a005547c
Marcos Spano <e.marcosspano@gmail.com>
2021-03-01 19:44:39 -0300

    Atualizando view Index

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var classesRouter = require('./routes/classes');
+var plansRouter = require('./routes/plans');
+var teachersRouter = require('./routes/teachers');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/classes', classesRouter);
+app.use('/plans', plansRouter);
+app.use('/teachers', teachersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
