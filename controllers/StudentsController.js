00f6f2626150eb5dc260cd8f7ad040304e703db8
Marcos Spano <e.marcosspano@gmail.com>
2021-03-06 23:44:40 -0300

    Alteração StudentsController.js

--- controllers/StudentsController.js
+++ controllers/StudentsController.js
@@ -1,8 +1,25 @@
-const { Student } = require("../models");
+const { Student, Teacher, Plan } = require("../models");
 
 module.exports = {
-    list (req, res, next) {
-        res.render('students');
+    async list (req, res, next) {
+        let students = await Student.findAll({
+            where: {
+                active: 1,
+                deleted: 0
+            }
+        });
+        let teachers = await Teacher.findAll({
+            where: {
+                active:1,
+                deleted: 0
+            }
+        });
+        let plans = await Plan.findAll({
+            active: 1,
+            deleted: 0
+        });
+
+        res.render('students', { students, teachers, plans, user: req.session.user });
     },
 
     async create (req, res, next) {
