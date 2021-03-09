70ad4c5e5aab94c6447bc6e0a6865b80c9fb395c
Marcos Spano <e.marcosspano@gmail.com>
2021-03-09 18:55:54 -0300

    Correções de Edição e Exclusão

--- controllers/StudentsController.js
+++ controllers/StudentsController.js
@@ -34,29 +34,27 @@ module.exports = {
 
         students.forEach(dado => {
             if (dado.id == full_name_students) {
-                dado_Student = { email: dado.email }
+                dado_Student = { email: dado.email, id: dado.id }
             }
         });
 
         classes.forEach(dado => {
             if (dado.id_students == full_name_students) {
-                dado_Student = { email: dado_Student.email, id_plan: dado.id_plans, id_teacher: dado.id_teachers }
+                dado_Student = { id: dado_Student.id, email: dado_Student.email, id_plan: dado.id_plans, id_teacher: dado.id_teachers }
             }
         });
 
         teachers.forEach(dado => {
             if (dado.id == dado_Student.id_teacher) {
-                dado_Student = { email: dado_Student.email, id_plan: dado_Student.id_plan, id_teacher: dado_Student.id_teacher, full_name: dado.full_name, speciality: dado.speciality}
+                dado_Student = { id: dado_Student.id, email: dado_Student.email, id_plan: dado_Student.id_plan, id_teacher: dado_Student.id_teacher, full_name: dado.full_name, speciality: dado.speciality}
             }
         });
 
         plans.forEach(dado => {
             if (dado.id == dado_Student.id_plan) {
-                dado_Student = { email: dado_Student.email, id_plan: dado_Student.id_plan, id_teacher: dado_Student.id_teacher, full_name: dado_Student.full_name, speciality: dado_Student.speciality, title: dado.title }
+                dado_Student = { id: dado_Student.id, email: dado_Student.email, id_plan: dado_Student.id_plan, id_teacher: dado_Student.id_teacher, full_name: dado_Student.full_name, speciality: dado_Student.speciality, title: dado.title }
             }
         });
-
-        // console.log (' E-MAIL: ' + dado_Student.email + ' ID PLANO: ' + dado_Student.id_plan + ' ID PROFESSOR: ' + dado_Student.id_teacher);
 
         res.render('students', { dado_Student, students, teachers, plans, user: req.session.user });
     },
@@ -72,17 +70,45 @@ module.exports = {
         
         let new_student = await Student.findOne( { where: { email: emailStudents } });
         await Classe.create ( { id_students: new_student.id, id_teachers: full_teachers_students, id_plans: full_plan_students } );
+
     },
 
     async edit (req, res, next) {
+        let id = req.params.id;
+        let student = await Student.findByPk(id);
 
+    res.render('students', students, { student });
     },
 
     async update (req, res, next) {
+        let id = req.params.id;
+        let student = await Student.findByPk(id);
+        let classes = await Classe.findOne({
+            where: {
+                id_students: id
+            }
+        });
 
+        let { emailStudents, full_plan_students, full_teachers_students } = req.body;
+
+        await student.update( { email: emailStudents } );
+        await classes.update({ id_teachers: full_teachers_students, id_plans: full_plan_students})
+
+        await student.save();
+        await classes.save();
+
+    res.render('students', { student, students, classes, updated: true })
     },
 
     async delete (req, res, next) {
+        let id = req.params.id;
+        let student = await Student.findByPk(id);
 
+        student.deleted = true;
+        student.active = false;
+
+        await student.save();
+    
+    res.render('students', students, student);
     }
 }
--- seeders/20210308100733-classes-seeder.js
+++ seeders/20210308100733-classes-seeder.js
@@ -8,6 +8,612 @@ module.exports = {
       id_plans: 2,
       createdAt: new Date(),
       updatedAt: new Date()
+    }, {
+      id_students: 2,
+      id_teachers: 4,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 3,
+      id_teachers: 4,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 4,
+      id_teachers: 3,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 5,
+      id_teachers: 1,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 6,
+      id_teachers: 2,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 7,
+      id_teachers: 3,
+      id_plans: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 8,
+      id_teachers: 4,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 9,
+      id_teachers: 2,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 10,
+      id_teachers: 1,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 11,
+      id_teachers: 4,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 12,
+      id_teachers: 3,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 13,
+      id_teachers: 2,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 14,
+      id_teachers: 1,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 15,
+      id_teachers: 1,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 16,
+      id_teachers: 2,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 17,
+      id_teachers: 3,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 18,
+      id_teachers: 4,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 19,
+      id_teachers: 4,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 20,
+      id_teachers: 2,
+      id_plans: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 21,
+      id_teachers: 1,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 22,
+      id_teachers: 3,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 23,
+      id_teachers: 2,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 24,
+      id_teachers: 4,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 25,
+      id_teachers: 1,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 26,
+      id_teachers: 2,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 27,
+      id_teachers: 3,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 28,
+      id_teachers: 4,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 29,
+      id_teachers: 4,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 30,
+      id_teachers: 3,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 31,
+      id_teachers: 2,
+      id_plans: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 32,
+      id_teachers: 1,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 33,
+      id_teachers: 1,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 34,
+      id_teachers: 1,
+      id_plans: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 35,
+      id_teachers: 1,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 36,
+      id_teachers: 4,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 37,
+      id_teachers: 4,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 38,
+      id_teachers: 4,
+      id_plans: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 39,
+      id_teachers: 4,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 40,
+      id_teachers: 4,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 41,
+      id_teachers: 3,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 42,
+      id_teachers: 3,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 43,
+      id_teachers: 3,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 44,
+      id_teachers: 3,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 45,
+      id_teachers: 2,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 46,
+      id_teachers: 2,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 47,
+      id_teachers: 2,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 48,
+      id_teachers: 2,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 49,
+      id_teachers: 2,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 50,
+      id_teachers: 1,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 51,
+      id_teachers: 1,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 52,
+      id_teachers: 1,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 53,
+      id_teachers: 2,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 54,
+      id_teachers: 3,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 55,
+      id_teachers: 4,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 56,
+      id_teachers: 4,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 57,
+      id_teachers: 3,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 58,
+      id_teachers: 2,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 59,
+      id_teachers: 1,
+      id_plans: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 60,
+      id_teachers: 1,
+      id_plans: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 61,
+      id_teachers: 2,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 62,
+      id_teachers: 3,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 63,
+      id_teachers: 4,
+      id_plans: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 64,
+      id_teachers: 4,
+      id_plans: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 65,
+      id_teachers: 3,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 66,
+      id_teachers: 2,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 67,
+      id_teachers: 1,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 68,
+      id_teachers: 4,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 69,
+      id_teachers: 3,
+      id_plans: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 70,
+      id_teachers: 2,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 71,
+      id_teachers: 1,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 72,
+      id_teachers: 1,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 73,
+      id_teachers: 2,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 74,
+      id_teachers: 3,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 75,
+      id_teachers: 4,
+      id_plans: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 76,
+      id_teachers: 3,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 77,
+      id_teachers: 2,
+      id_plans: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 78,
+      id_teachers: 1,
+      id_plans: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 79,
+      id_teachers: 1,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 80,
+      id_teachers: 1,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 81,
+      id_teachers: 2,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 82,
+      id_teachers: 1,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 83,
+      id_teachers: 2,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 84,
+      id_teachers: 3,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 85,
+      id_teachers: 4,
+      id_plans: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 86,
+      id_teachers: 4,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 87,
+      id_teachers: 4,
+      id_plans: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 88,
+      id_teachers: 3,
+      id_plans: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 89,
+      id_teachers: 4,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 90,
+      id_teachers: 3,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 91,
+      id_teachers: 2,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 92,
+      id_teachers: 4,
+      id_plans: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 93,
+      id_teachers: 3,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 94,
+      id_teachers: 2,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 95,
+      id_teachers: 1,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 96,
+      id_teachers: 1,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 97,
+      id_teachers: 2,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 98,
+      id_teachers: 3,
+      id_plans: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 99,
+      id_teachers: 4,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 100,
+      id_teachers: 3,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 101,
+      id_teachers: 2,
+      id_plans: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id_students: 102,
+      id_teachers: 1,
+      id_plans: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
     }]);
   },
 
