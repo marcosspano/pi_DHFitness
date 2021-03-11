68c131cc844642b1f642e350d819b298f60bce02
Aluízio <aluizio2210@gmail.com>
2021-03-10 23:48:13 -0500

    Alteração no letreiro dos planos

---
+++ config/database_modelo.js
@@ -0,0 +1,9 @@
+const config = {
+    username: 'usuario_banco',
+    password: 'senha_banco',
+    database: 'nome_banco',
+    host: 'host_banco',
+    dialect: 'mysql'
+  }
+
+module.exports = config;
